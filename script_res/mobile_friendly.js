67873bdca29111047b07ff3cfbcc3b174ddbf041
Vuanotto <43342818+Vuanotto@users.noreply.github.com>
2023-10-22 18:00:08 +1100

    DARKNESS

--- script_res/mobile_friendly.js
+++ script_res/mobile_friendly.js
@@ -33,12 +33,14 @@ function SwitchCSSMode()
     // elements. 
     // Select your element using indexing. 
     var theme = document.getElementsByTagName('link')[1];
-
+    var button = document.getElementById('switchTheme');
     // Change the value of href attribute  
     // to change the css sheet. 
-    if (theme.getAttribute('href') == 'mobileFriendlyLight.css') {
-        theme.setAttribute('href', 'mobileFriendlyDark.css');
+    if (theme.getAttribute('href') == 'script_res/mobileFriendlyLight.css') {
+        theme.setAttribute('href', 'script_res/mobileFriendlyDark.css');
+        button.textContent = "Light Theme";
     } else {
-        theme.setAttribute('href', 'mobileFriendlyLight.css');
+        theme.setAttribute('href', 'script_res/mobileFriendlyLight.css');
+        button.textContent = "Dark Theme";
     }
 }
